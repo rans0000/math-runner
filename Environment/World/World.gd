fda14610297304f8cb67d90399337bf94a68313d
rans0000 <rans0000@gmail.com>
2021-03-07 20:54:07 +0530

    connect player signals based on group in world scene

--- Environment/World/World.gd
+++ Environment/World/World.gd
@@ -6,15 +6,19 @@ const BLOCK_WIDTH = 50
 onready var pause_menu = $PauseMenu
 onready var Global = get_node("/root/Global")
 onready var desert_scene = preload("res://Environment/Desert/Desert.tscn")
-onready var player = $Player
+#onready var player = $Player
 
 
 
 func _ready():
 	pause_menu.visible = false
 	prints("Global number", Global.number)
-	player.connect("detect_empty_floor", self, "generate_world")
-	player.connect("detect_obsolete_floor", self, "delete_floor")
+#	player.connect("detect_empty_floor", self, "generate_world")
+#	player.connect("detect_obsolete_floor", self, "delete_floor")
+	for player in get_tree().get_nodes_in_group("player"):
+		player.connect("detect_empty_floor", self, "generate_world")
+		player.connect("detect_obsolete_floor", self, "delete_floor")
+	
 	for i in range(4):
 		generate_world()
 
@@ -37,5 +41,6 @@ func generate_world():
 
 
 func delete_floor(pos_z, platform):
+	return
 	platform.queue_free()
 	pass
